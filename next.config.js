11a2200b47d7fbf5efabbad1a337879c195f83a8
andres.developer19 <andres.developer19@gmail.com>
2023-09-03 15:25:06 -0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+const nextConfig = {
+    images: {
+        domains: ['fakestoreapi.com','tailwindui.com','localhost:3000','images-na.ssl-images-amazon.com']
+      },
+}
 module.exports = nextConfig
